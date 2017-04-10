f1f460739b729e09bc77664193ff26e71739b5ff
Felipe <Felipe@Felipe-PC>
2017-04-10 17:10:41 -0500

    Registro con correo Pagos

--- src/java/com/futbolweb/pagos/beans/RegistroPagoManagedBean.java
+++ src/java/com/futbolweb/pagos/beans/RegistroPagoManagedBean.java
@@ -5,9 +5,11 @@
  */
 package com.futbolweb.pagos.beans;
 
+import com.futbolweb.backend.entities.Jugador;
 import com.futbolweb.backend.entities.Pago;
 import com.futbolweb.backend.facade.PagoFacade;
 import com.futbolweb.beans.JugadorManagedBean;
+import com.futbolweb.beans.UsuarioManagedBean;
 import com.futbolweb.email.Email;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
@@ -27,7 +29,10 @@ public class RegistroPagoManagedBean {
      @EJB 
     private PagoFacade pagof;
     @Inject
+    private UsuarioManagedBean usuarioC;
+    @Inject
     private JugadorManagedBean jugadorC;
+    
     @PostConstruct
     public void init(){
     pago = new Pago();
@@ -40,11 +45,29 @@ public class RegistroPagoManagedBean {
     public void setPago(Pago pago) {
         this.pago = pago;
     }
+
+    public UsuarioManagedBean getUsuarioC() {
+        return usuarioC;
+    }
+
+    public void setUsuarioC(UsuarioManagedBean usuarioC) {
+        this.usuarioC = usuarioC;
+    }
+
+    public JugadorManagedBean getJugadorC() {
+        return jugadorC;
+    }
+
+    public void setJugadorC(JugadorManagedBean jugadorC) {
+        this.jugadorC = jugadorC;
+    }
+
+    
     
     public void registrarPago(){
     Email envioC;
-    envioC = new Email("Novedad de pago Expreso Rojo", "Se le notifíca que se le ha generado un nuevo registro de pago en el club Expreso Rojo, para mas información consultar el control de pagos en nuestro sistema :",jugadorC.getJugador().getUsuario().getCorreo());
-    envioC.enviarEmail2();
+    envioC = new Email("Novedad de pago Expreso Rojo", "Se le notifíca que se le ha generado un nuevo registro de pago en el club Expreso Rojo, para mas información consultar el control de pagos en nuestro sistema :", pago.getFkIdJugador().getUsuario().getCorreo());
+    envioC.enviarEmail();
     pagof.create(pago);
     }
     /**
